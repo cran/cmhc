a7fd9eccbea5cd44e865d8d4db4bf52dfb8a2f0d
Jens von Bergmann <jens@mountainmath.ca>
2023-04-02 21:30:19 +0000

    version 0.2.5

--- inst/doc/rental_universe.R
+++ inst/doc/rental_universe.R
@@ -1,7 +1,8 @@
 ## ---- include = FALSE---------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
-  comment = "#>"
+  comment = "#>",
+  eval = nzchar(Sys.getenv("COMPILE_VIG"))
 )
 
 ## ----setup--------------------------------------------------------------------
